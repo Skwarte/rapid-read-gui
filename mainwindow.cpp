89b0562ddf543c7e1d39d4dea99fec2ee9b8ed24
skwarte <skwarte@purdue.edu>
2017-06-02 12:53:33 -0400

    window displays words at a set WPM speed.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@
 #include <QDebug>
 #include <QFile>
 #include <QMessageBox>
+#include <unistd.h>
+#include <QThread>
+#include "qapplication.h"
 
 //#include <QListWidget>
 //#include <QListWidgetItem>
@@ -25,10 +28,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-  //  QString desk = (QString)QStandardPaths::DesktopLocation;
+    //  QString desk = (QString)QStandardPaths::DesktopLocation;
     QString desktop = QStandardPaths::locate(QStandardPaths::DesktopLocation, QString(), QStandardPaths::LocateDirectory);
     //qDebug() << QStandardPaths::DesktopLocation;
-    fileName = QFileDialog::getOpenFileName(this, tr("Open File"), desktop, tr("txt files (*.txt)"));
+    fileName = QFileDialog::getOpenFileName(this, tr("Open File"), desktop, tr("txt files (*.txt)"), 0, QFileDialog::DontUseNativeDialog);
     qDebug() << fileName;
     //ui->listWidget->addItems(filenames);
 
@@ -38,12 +41,27 @@ void MainWindow::on_pushButton_clicked()
     QTextStream in(&file);
     QString word = in.readAll();
     QStringList fileWords = word.split(" ");
+//    ui->currWord->setText(fileWords[0]);
+//    QThread::sleep(7);
+//        ui->currWord->repaint();
+//    ui->currWord->setText(fileWords[1]);
+//    QThread::sleep(7);
+//        ui->currWord->repaint();
+//    ui->currWord->setText(fileWords[2]);
+//        ui->currWord->repaint();
+//        QThread::sleep(1);
 
-    for(int i = 0; i < fileWords.length(); i++)
-    {
-        qDebug() << fileWords[i];
-    }
-    //file.open(fileName);
-
+        for(int i = 0; i < fileWords.length(); i++)
+        {
+             qDebug() << fileWords[i];
+            ui->currWord->setText(fileWords[i]);
+            qApp->processEvents();
+            QThread::usleep(60000000/450); //second spent on each word
+        }
+//        QThread::sleep(2);
+//        ui->currWord->hide();
+//        QThread::sleep(2);
+//        ui->currWord->setText("End of file has been reached. Please select another file.");
+//        ui->currWord->show();
 }
 
