d09139aeda257bf9449bbd0093e357dcdc1f7785
skwarte <skwarte@purdue.edu>
2017-06-05 19:52:37 -0400

    user can chose the words per minute rate

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@
 #include <QDebug>
 #include <QFile>
 #include <QMessageBox>
+#include <QStringRef>
 #include <unistd.h>
 #include <QThread>
 #include "qapplication.h"
@@ -22,6 +23,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QFont font = ui->currWord->font();
     font.setPointSize(40);
     ui->currWord->setFont(font);
+    ui->WPM->hide();
 }
 
 MainWindow::~MainWindow()
@@ -38,10 +40,20 @@ void MainWindow::on_pushButton_clicked()
         QStringList fileName = fileDir.split("/");
         ui->currWord->setText("Selected File: " + fileName[fileName.length()-1]);
         ui->pushButton->setText("Go!");
+        ui->WPM->show();
     }
     else
     {
             ui->pushButton->hide();
+            ui->WPM->hide();
+            QString spw = ui->WPM->currentText();
+            spw = spw.mid(0, 3);
+            int wpm = spw.toInt();
+           // QStringRef subs(spw, 0, 3);//words per minute traslated from string to int
+
+            //QString spw2 = ui->WPM->
+            qDebug() << spw;
+           // secondPerWord
             QFile file(fileDir);
             file.open(QIODevice::ReadOnly | QIODevice::Text);
             QTextStream in(&file);
@@ -53,7 +65,7 @@ void MainWindow::on_pushButton_clicked()
                 qDebug() << fileWords[i];
                 ui->currWord->setText(fileWords[i]);
                 qApp->processEvents();
-                QThread::usleep(60000000/450); //second spent on each word
+                QThread::usleep(60000000/wpm); //second spent on each word
             }
             ui->pushButton->setText("Select Another File");
             ui->pushButton->show();
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,7 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     QString fileName;
     QString fileDir;
+    int secondPerWord;
     ~MainWindow();
 
 private slots:
